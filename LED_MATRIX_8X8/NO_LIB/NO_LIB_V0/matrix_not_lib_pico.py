cecd8381eddf668136145cd5372676f9c2c01a38
Alessio Menchetti <167858968+secoAutomationGithub@users.noreply.github.com>
2024-08-14 15:15:57 +0200

    Modifiche matrice 8x8

--- LED_MATRIX_8X8/NO_LIB/NO_LIB_V0/matrix_not_lib_pico.py
+++ LED_MATRIX_8X8/NO_LIB/NO_LIB_V0/matrix_not_lib_pico.py
@@ -1,20 +1,19 @@
-# Write your code here :-)
 from machine import Pin, SPI
 import time
 
-# Configurazione della SPI
+# Configurazione della SPI ---- Configuration SPI
 spi = SPI(0, baudrate=10000000, polarity=0, phase=0, sck=Pin(18), mosi=Pin(19))
 
-# Configurazione del pin CS
+# Configurazione del pin CS ---- Configuration pin CS (chip select)
 cs = Pin(17, Pin.OUT)
 
-# Funzione per inviare un comando al MAX7219
+# Funzione per inviare un comando al MAX7219 ---- Function to send command MAX7219
 def send_cmd(register, data):
     cs.off()
     spi.write(bytearray([register, data]))
     cs.on()
 
-# Inizializzazione del MAX7219
+# Inizializzazione del MAX7219 architettura ---- Initialization MAX7219 architecture
 def init_max7219():
     send_cmd(0x09, 0x00)  # Decoding mode: no decoding
     send_cmd(0x0A, 0x03)  # Intensity: 3 (range 0x00 to 0x0F)
@@ -22,12 +21,12 @@ def init_max7219():
     send_cmd(0x0C, 0x01)  # Shutdown register: normal operation
     send_cmd(0x0F, 0x00)  # Display test: off
 
-# Funzione per pulire la matrice
+# Funzione per pulire la matrice ---- Function to clean the matrix
 def clear_matrix():
     for i in range(8):
         send_cmd(i + 1, 0x00)
 
-# Funzione per visualizzare un carattere sulla matrice
+# Funzione per visualizzare un carattere sulla matrice ---- Function to visualize character on matrix
 def display_char(char):
     font = {
         'A': [0x3E, 0x09, 0x09, 0x3E, 0x00, 0x00, 0x00, 0x00],
@@ -42,15 +41,14 @@ def display_char(char):
         for i, value in enumerate(font[char]):
             send_cmd(i + 1, value)
 
-# Inizializzazione del MAX7219
+# Inizializzazione del MAX7219 ---- Initialization MAX7219 
 init_max7219()
 clear_matrix()
 
-# Loop principale per visualizzare lettere e numeri
 characters = ['A', 'B', 'C', '1', '2', '3']
-
+# Loop program
 while True:
     for char in characters:
         clear_matrix()
         display_char(char)
-        time.sleep(1)  # Visualizza ogni carattere per 1 secondo
+        time.sleep(1)  # Delay 
